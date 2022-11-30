16183d074b8f0c81e65ac574b0de348fb81a2165
João Pedro <jp.bruno.machado@gmail.com>
2022-11-30 12:39:51 -0300

    added right color to dots

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,7 +1,7 @@
 import { HStack, Divider } from "native-base";
 
 const Dot = ({ section, currentSection }) => {
-  return <Divider width={section === currentSection ? "32px" : "12px"} height={3} borderRadius={100} backgroundColor={section === currentSection ? "yellow.500" : "white"} />;
+  return <Divider width={section === currentSection ? "32px" : "12px"} height={3} borderRadius={100} backgroundColor={section === currentSection ? "#FAFF00" : "white"} />;
 };
 
 export default function ProgressBar({ section }) {
